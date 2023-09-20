6ec9a0f5fe404bd56b1b49d7b3f147fb0e8364b5
dpletzke <dpletzke@gmail.com>
2023-09-19 22:27:18 -0500

    chore(useDb.ts): comment out unused code in useDb hook for better code readability and maintainability

--- hooks/useDb.ts
+++ hooks/useDb.ts
@@ -1,67 +1,67 @@
-import { useCallback, useEffect } from "react";
-import { useObject, useRealm, useQuery } from "../schemas";
-import { Notifier } from "../schemas/NotifierSchema";
-import { useUser } from "@realm/react";
+// import { useCallback, useEffect } from "react";
+// import { useObject, useRealm, useQuery } from "../schemas";
+// import { Notifier } from "../schemas/NotifierSchema";
+// import { useUser } from "@realm/react";
 
-export const useDb = () => {
-  const realm = useRealm();
-  const notifiers = useQuery(Notifier).sorted("_id");
-  const user = useUser();
+// export const useDb = () => {
+//   const realm = useRealm();
+//   const notifiers = useQuery(Notifier).sorted("_id");
+//   const user = useUser();
 
-  useEffect(() => {
-    realm.subscriptions.update((mutableSubs) => {
-      mutableSubs.add(realm.objects(Notifier));
-    });
-  }, [realm, user]);
+//   useEffect(() => {
+//     realm.subscriptions.update((mutableSubs) => {
+//       mutableSubs.add(realm.objects(Notifier));
+//     });
+//   }, [realm, user]);
 
-  const createNotifier = useCallback(
-    (stationId: string, threshold: number): void => {
-      if (!stationId || !threshold) {
-        return;
-      }
-      realm.write(() => {
-        realm.create(Notifier, {
-          stationId,
-          threshold,
-        });
-      });
-    },
-    [realm]
-  );
+//   const createNotifier = useCallback(
+//     (stationId: string, threshold: number): void => {
+//       if (!stationId || !threshold) {
+//         return;
+//       }
+//       realm.write(() => {
+//         realm.create(Notifier, {
+//           stationId,
+//           threshold,
+//         });
+//       });
+//     },
+//     [realm]
+//   );
 
-  const getNotifiers = useCallback(() => {
-    return realm.objects<Notifier>(Notifier.name);
-  }, [realm]);
+//   const getNotifiers = useCallback(() => {
+//     return realm.objects<Notifier>(Notifier.name);
+//   }, [realm]);
 
-  const getNotifierById = useCallback(
-    (id: string) => {
-      return useObject<Notifier>(Notifier.name, id);
-    },
-    [realm]
-  );
+//   const getNotifierById = useCallback(
+//     (id: string) => {
+//       return useObject<Notifier>(Notifier.name, id);
+//     },
+//     [realm]
+//   );
 
-  const deleteNotifier = useCallback(
-    (id: string) => {
-      const notifier = getNotifierById(id);
-      realm.write(() => {
-        realm.delete(notifier);
-      });
-    },
-    [realm]
-  );
+//   const deleteNotifier = useCallback(
+//     (id: string) => {
+//       const notifier = getNotifierById(id);
+//       realm.write(() => {
+//         realm.delete(notifier);
+//       });
+//     },
+//     [realm]
+//   );
 
-  const deleteAllNotifiers = useCallback(() => {
-    realm.write(() => {
-      realm.delete(getNotifiers());
-    });
-  }, [realm]);
+//   const deleteAllNotifiers = useCallback(() => {
+//     realm.write(() => {
+//       realm.delete(getNotifiers());
+//     });
+//   }, [realm]);
 
-  return {
-    realm,
-    user,
-    notifiers,
-    createNotifier,
-    deleteNotifier,
-    deleteAllNotifiers,
-  };
-};
+//   return {
+//     realm,
+//     user,
+//     notifiers,
+//     createNotifier,
+//     deleteNotifier,
+//     deleteAllNotifiers,
+//   };
+// };
