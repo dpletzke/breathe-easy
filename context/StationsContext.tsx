752f3cb8ba795c81e8e8c1f83aa3c9edef71a7cd
dpletzke <dpletzke@gmail.com>
2023-09-19 22:31:02 -0500

    refactor(StationsContext.tsx): - Remove unused import of useContext from react - Add StationRecord type import from types - Add mergeData function import from utils/helpers - Change type declaration of StationsType to use string as key instead of number - Change setStationData function name to setStationResponses for better semantics - Refactor setStationLookups function to use reduce instead of forEach for better immutability and readability - Refactor deleteStation function to use immutability when deleting a station from the stations object - Comment out setStationData function as it is currently unused

--- context/StationsContext.tsx
+++ context/StationsContext.tsx
@@ -1,25 +1,22 @@
-import React, { createContext, useContext, useState } from "react";
-import { StationLookup, StationResponse } from "../types";
+import React, { createContext, useState } from "react";
+import { StationLookup, StationResponse, StationRecord } from "../types";
+import { mergeData } from "../utils/helpers";
 
-export type StationRecord = {
-  lookup: StationLookup;
-  data: (StationResponse["data"] & { time: StationResponse["time"] }) | null;
-};
-
-type StationsType = { [key in string]: StationRecord };
+type StationsType = { [key: string]: StationRecord };
 
 export type StationsContextType = {
   stations: StationsType;
   setStationLookups: (lookups: StationLookup[]) => void;
   deleteStation: (uid: string) => void;
-  setStationData: (responses: StationResponse[]) => void;
+  setStationResponses: (reses: StationResponse[]) => void;
 };
 
 export const StationsContext = createContext<StationsContextType>({
   stations: {},
   setStationLookups: () => {},
   deleteStation: () => {},
-  setStationData: () => {},
+  setStationResponses: () => {},
+  // setStationData: () => {},
 });
 
 export const StationsProvider = ({
@@ -30,24 +27,45 @@ export const StationsProvider = ({
   const [stations, setStations] = useState<StationsType>({});
 
   const setStationLookups = (lookups: StationLookup[]) => {
-    lookups.forEach((lookup) => {
-      stations[`${lookup.uid}`] = { lookup, data: null };
+    const newLookups = lookups.reduce((acc, { uid, station }) => {
+      acc[`${uid}`] = { stationId: `${uid}`, name: station.name };
+      return acc;
+    }, {} as StationsType);
+    setStations((prev) => mergeData(prev, newLookups));
+  };
+
+  const setStationResponses = (reses: StationResponse[]) => {
+    const newReses = reses.reduce((acc, { data }) => {
+      const { idx, ...rest } = data;
+      acc[`${idx}`] = { stationId: `${idx}`, name: rest.city.name };
+      return acc;
+    }, {} as StationsType);
+    setStations((prev) => mergeData(prev, newReses));
+  };
+
+  const deleteStation = (stationId: string) => {
+    setStations((prev) => {
+      const newState = { ...prev };
+      delete newState[stationId];
+      return newState;
     });
   };
-  const deleteStation = (uid: string) => {
-    delete stations[uid];
-  };
-  const setStationData = (responses: StationResponse[]) => {
-    responses.forEach((res) => {
-      const station = stations[`${res.data.idx}`];
-      if (station) {
-        station.data = { ...res.data, time: res.time };
-      }
-    });
-  };
+  // const setStationData = (responses: StationResponse[]) => {
+  //   responses.forEach((res) => {
+  //     const station = stations[`${res.data.idx}`];
+  //     if (station) {
+  //       station.data = { ...res.data, time: res.time };
+  //     }
+  //   });
+  // };
   return (
     <StationsContext.Provider
-      value={{ stations, setStationLookups, setStationData, deleteStation }}
+      value={{
+        stations,
+        setStationLookups,
+        setStationResponses,
+        deleteStation,
+      }}
     >
       {children}
     </StationsContext.Provider>
